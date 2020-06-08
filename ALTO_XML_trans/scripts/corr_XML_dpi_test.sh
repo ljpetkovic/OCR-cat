4e804efde79fb9196312ae85f3d63a5ed8bcbdfd
ljpetkovic <ljuca94@gmail.com>
2020-06-08 13:00:39 +0200

    Remove ALTO-XML_trans folder

--- ALTO_XML_trans/scripts/corr_XML_dpi_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#for g in /Users/ljudmilapetkovic/Desktop/Katabase/OCRcat/ALTO_XML_trans/doc/*
-#do
-	
-	input_file=../test/1871_08_RDA_N028-1.xml
-	input_image=../test/1871_08_RDA_N028-1.jpg
-	echo "Processing $input_file..." | sed 's/..\/test\///g'
-	python3 corr_XML_dpi_test.py $input_file $(convert $input_image -format "%x" info:) #$f is the file name, here '1871_08_RDA_N028-1.xml'
-	echo Done
