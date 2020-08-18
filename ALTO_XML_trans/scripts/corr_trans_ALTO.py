244a1646ab8018f41ff5d0240f1f911b42346760
ljpetkovic <ljuca94@gmail.com>
2020-08-18 18:34:32 +0200

    Removed the dpi conversion from the scripts and corrected the namespace

--- ALTO_XML_trans/scripts/corr_trans_ALTO.py
+++ ALTO_XML_trans/scripts/corr_trans_ALTO.py
@@ -9,7 +9,7 @@ fichier = sys.argv[1] #'1885_12_RDA_N095-1.xml'  # '1871_08_RDA_N028-1.xml'
  
 ########### récupération du dpi #######################
 
-dpi = int(sys.argv[2])
+#dpi = int(sys.argv[2])
 
 ########### récupération du nom du fichier #######################
 
@@ -29,14 +29,14 @@ root = tree.getroot()
 
  
 sourceImageInformationText = """
-      <sourceImageInformation xmlns="http://www.loc.gov/standards/alto/ns-v2#">
+      <sourceImageInformation>
          <fileName>{}</fileName>
       </sourceImageInformation>
 
 """.format(fileName)
 
 processingSoftwareText = """
-            <processingSoftware xmlns="http://www.loc.gov/standards/alto/ns-v2#">
+            <processingSoftware>
                <softwareCreator>CONTRIBUTORS</softwareCreator>
                <softwareName>pdfalto</softwareName>
                <softwareVersion>0.1</softwareVersion>
@@ -45,7 +45,7 @@ processingSoftwareText = """
 sourceImageInformation = etree.fromstring(sourceImageInformationText)
 processingSoftware = etree.fromstring(processingSoftwareText)
 for processing_software in root[0][1]:
-    for p in processing_software.findall('{http://www.loc.gov/standards/alto/ns-v2#}processingSoftware'):
+    for p in processing_software.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}processingSoftware'):
         processing_software.remove(p)
 
     
@@ -54,24 +54,24 @@ root[0][2][0].insert(1, processingSoftware)
 
 #############   nettoyage du ficher des balises inutiles     ################
 
-tag_a_supprimer = ['{http://www.loc.gov/standards/alto/ns-v2#}TopMargin',
-                   '{http://www.loc.gov/standards/alto/ns-v2#}LeftMargin',
-                   '{http://www.loc.gov/standards/alto/ns-v2#}RightMargin',
-                   '{http://www.loc.gov/standards/alto/ns-v2#}BottomMargin']
+tag_a_supprimer = ['{http://www.loc.gov/standards/alto/v3/alto.xsd}TopMargin',
+                   '{http://www.loc.gov/standards/alto/v3/alto.xsd}LeftMargin',
+                   '{http://www.loc.gov/standards/alto/v3/alto.xsd}RightMargin',
+                   '{http://www.loc.gov/standards/alto/v3/alto.xsd}BottomMargin']
 
-for page in root[2].iter('{http://www.loc.gov/standards/alto/ns-v2#}Page'):
+for page in root[2].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
     for tag in tag_a_supprimer:
         for elem in page.findall(tag):
             page.remove(elem)
-    for printspace in page.findall('{http://www.loc.gov/standards/alto/ns-v2#}PrintSpace'):
-        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/ns-v2#}TextBlock'):
-            for shape in textblock.findall('{http://www.loc.gov/standards/alto/ns-v2#}Shape'):
+    for printspace in page.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}PrintSpace'):
+        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextBlock'):
+            for shape in textblock.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}Shape'):
                 textblock.remove(shape)
 
 ############    ajout d'une balise <Styles> avec les polices dans l'en-tête     ##########
 
 stylesText = """
-<Styles xmlns="http://www.loc.gov/standards/alto/ns-v2#">
+<Styles>
      <TextStyle ID="FONT0"
                 FONTFAMILY="unknown"
                 FONTSIZE="unknown"
@@ -102,13 +102,13 @@ root.insert(1, styles)
 
 #######  Récupération et création incrémentale des ID des éléments <String> à partir de l'ID des éléments <TextLine> #########
 
-for page in root[3].iter('{http://www.loc.gov/standards/alto/ns-v2#}Page'):
-    for printspace in page.findall('{http://www.loc.gov/standards/alto/ns-v2#}PrintSpace'):
-        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/ns-v2#}TextBlock'):
+for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
+    for printspace in page.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}PrintSpace'):
+        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextBlock'):
             id_tex_block = textblock.attrib['ID']
-            for textline in textblock.findall('{http://www.loc.gov/standards/alto/ns-v2#}TextLine'):
+            for textline in textblock.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextLine'):
                 id_textline = textline.attrib['ID']
-                for i, string in enumerate(textline.findall('{http://www.loc.gov/standards/alto/ns-v2#}String'),
+                for i, string in enumerate(textline.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}String'),
                                            start=1):
                     string.set('ID', id_textline + "_{}".format(str(i)))
 
@@ -121,11 +121,11 @@ patt_b_closed = r'(([<]*[ ]*[\/][ ]*b([ >])*)+[ >.,;])|(([<]*[ ]*[\/][ ]*b([ >])
 patt_i_closed = r'(([<]*[ ]*[\/][ ]*i([ >])*)+[ >.,;])|(([<]*[ ]*[\/][ ]*i([ >])*)+$)|[ <i>]*<[ \/<i>]*i([ <i>]*>[ <i>]*)*'
 
 
-for page in root[3].iter('{http://www.loc.gov/standards/alto/ns-v2#}Page'):
-    for printspace in page.findall('{http://www.loc.gov/standards/alto/ns-v2#}PrintSpace'):
-        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/ns-v2#}TextBlock'):
-            for textline in textblock.findall('{http://www.loc.gov/standards/alto/ns-v2#}TextLine'):
-                for string in textline.findall('{http://www.loc.gov/standards/alto/ns-v2#}String'):
+for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
+    for printspace in page.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}PrintSpace'):
+        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextBlock'):
+            for textline in textblock.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextLine'):
+                for string in textline.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}String'):
                     if re.search(patt_b_open,string.attrib['CONTENT']): 
                         string.attrib['CONTENT'] = re.sub(patt_b_open,'<b>',string.attrib['CONTENT'])
 ##                        #print(string.attrib['CONTENT'])
@@ -143,13 +143,13 @@ for page in root[3].iter('{http://www.loc.gov/standards/alto/ns-v2#}Page'):
 
 #### Application des trois styles (FONT0, FONT1, FONT2) à tous les éléments <String>	######
 
-for page in root[3].iter('{http://www.loc.gov/standards/alto/ns-v2#}Page'):
-    for printspace in page.findall('{http://www.loc.gov/standards/alto/ns-v2#}PrintSpace'):
-        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/ns-v2#}TextBlock'):
-            for textline in textblock.findall('{http://www.loc.gov/standards/alto/ns-v2#}TextLine'):
+for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
+    for printspace in page.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}PrintSpace'):
+        for textblock in printspace.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextBlock'):
+            for textline in textblock.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextLine'):
                 start_b = False
                 start_i = False
-                for string in textline.findall('{http://www.loc.gov/standards/alto/ns-v2#}String'):
+                for string in textline.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}String'):
                     string.set('STYLEREFS', 'FONT0')
                     if '<b>' in string.attrib['CONTENT']:
                         start_b = True
@@ -170,14 +170,14 @@ for page in root[3].iter('{http://www.loc.gov/standards/alto/ns-v2#}Page'):
 
 ###### Conversion mm10 en pixels #######
 
-liste_attribut = ["HPOS", "VPOS", "HEIGHT", "WIDTH"]
-for elt in root.iter():
-    dic = elt.attrib
-    for l in liste_attribut:
-        if l in dic.keys():
-            mm10 = dic[l]
-            pixels = str(int(mm10) * dpi / 254)
-            elt.set(l, pixels)
+##liste_attribut = ["HPOS", "VPOS", "HEIGHT", "WIDTH"]
+##for elt in root.iter():
+##    dic = elt.attrib
+##    for l in liste_attribut:
+##        if l in dic.keys():
+##            mm10 = dic[l]
+##            pixels = str(int(mm10) * dpi / 254)
+##            elt.set(l, pixels)
 
 tree.write(fichier + '_trans.xml', encoding='utf8', pretty_print=True)
 
