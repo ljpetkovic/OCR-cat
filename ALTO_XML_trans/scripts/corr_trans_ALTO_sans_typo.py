f2629219e08080e1b3e4bbd749db754500bc8b1d
ljpetkovic <ljuca94@gmail.com>
2020-08-24 09:04:43 +0200

    Added the revised training results for LAC and LAV

--- ALTO_XML_trans/scripts/corr_trans_ALTO_sans_typo.py
+++ ALTO_XML_trans/scripts/corr_trans_ALTO_sans_typo.py
@@ -5,11 +5,11 @@ import re
 
 ########### récupération du chemin du fichier #######################
 
-fichier = sys.argv[1] #'ALTO_demo.xml' #'1885_12_RDA_N095-1.xml'  # '1871_08_RDA_N028-1.xml'
+fichier = sys.argv[1] #'1885_12_RDA_N095-1.xml'  # '1871_08_RDA_N028-1.xml'
  
 ########### récupération du dpi #######################
 
-#dpi = int(sys.argv[2]) # 254
+#dpi = int(sys.argv[2])
 
 ########### récupération du nom du fichier #######################
 
@@ -21,8 +21,8 @@ fileName = result.group()
 ##################################################################
 
 
-parser_XML = etree.XMLParser(remove_blank_text=True) # </Styles> - la nouvelle ligne - <Tags/> - l'indentation  
-tree = etree.parse(fichier,parser_XML)
+##parser_XML = etree.XMLParser(remove_blank_text=True) # </Styles> - la nouvelle ligne - <Tags/> - l'indentation  
+tree = etree.parse(fichier)
 root = tree.getroot()
 
 ############## ajout des namespaces et des méta-données dans l'en-tête ##############
@@ -32,6 +32,7 @@ sourceImageInformationText = """
       <sourceImageInformation>
          <fileName>{}</fileName>
       </sourceImageInformation>
+
 """.format(fileName)
 
 processingSoftwareText = """
@@ -70,13 +71,9 @@ for page in root[2].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
 ############    ajout d'une balise <Styles> avec les polices dans l'en-tête     ##########
 
 stylesText = """
-<Styles xmlns="http://www.loc.gov/standards/alto/v3/alto.xsd">
-     <TextStyle ID="FONT0" 
+<Styles>
+     <TextStyle ID="FONT0"
                 FONTSTYLE=""/>
-     <TextStyle ID="FONT1"
-                FONTSTYLE="bold"/>
-     <TextStyle ID="FONT2"
-                FONTSTYLE="italics"/>
 </Styles>
 """
 
@@ -86,7 +83,7 @@ root.insert(1, styles)
 
 #######  Récupération et création incrémentale des ID des éléments <String> à partir de l'ID des éléments <TextLine> #########
 
-for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
+for page in root[2].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
     for printspace in page.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}PrintSpace'):
         for textblock in printspace.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextBlock'):
             id_tex_block = textblock.attrib['ID']
@@ -98,100 +95,34 @@ for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
 
                     
 
-####### Correction des balises pleines dans les fichiers ALTO-XML ##########
-patt_b_open = r'([ <](([< ])*b([ >])*)+[>])|([<](([< ])*b([ >])*)+[ >])|(^(([< ])*b([ >])*)+[>])|[ <b>]*<[ <b>]*b([ <b>]*>[ <b>]*)*|(?<=[(b>)])b>'
-patt_i_open = r'([ <](([< ])*i([ >])*)+[>])|([<](([< ])*i([ >])*)+[ >])|(^(([< ])*i([ >])*)+[>])|[ <i>]*<[ <i>]*i([ <i>]*>[ <i>]*)*|(?<=[(i>)])i>'
-patt_b_closed = r'(([<]*[ ]*[\/][ ]*b([ >])*)+[ >.,;])|(([<]*[ ]*[\/][ ]*b([ >])*)+$)|[ <b>]*<[ <b>]*[\/]b([ <b>]*>[ <b>]*)*|(?<=[(b>)])\/b>'
-patt_i_closed = r'(([<]*[ ]*[\/][ ]*i([ >])*)+[ >.,;])|(([<]*[ ]*[\/][ ]*i([ >])*)+$)|[ <i>]*<[ <i>]*[\/]i([ <i>]*>[ <i>]*)*|(?<=[(i>)])\/i>'
-pattern_regex_gen = re.compile(r'([<]+([<]*[ ]*[/]*[bi]*[ ]*[>]*)*)|(([<]*[ ]*[/]*[bi]*[ ]*[>]*)*[>]+)')
+##### Correction des balises pleines dans les fichiers ALTO-XML ##########
+patt_b_open = r'([ <](([< ])*b([ >])*)+[>])|([<](([< ])*b([ >])*)+[ >])|(^(([< ])*b([ >])*)+[>])|[ <b>]*<[ <b>]*b([ <b>]*>[ <b>]*)*'
+patt_i_open = r'([ <](([< ])*i([ >])*)+[>])|([<](([< ])*i([ >])*)+[ >])|(^(([< ])*i([ >])*)+[>])|[ <i>]*<[ <i>]*i([ <i>]*>[ <i>]*)*'
+patt_b_closed = r'(([<]*[ ]*[\/][ ]*b([ >])*)+[ >.,;])|(([<]*[ ]*[\/][ ]*b([ >])*)+$)|[ <b>]*<[ <b>]*[\/]b([ <b>]*>[ <b>]*)*'
+patt_i_closed = r'(([<]*[ ]*[\/][ ]*i([ >])*)+[ >.,;])|(([<]*[ ]*[\/][ ]*i([ >])*)+$)|[ <i>]*<[ \/<i>]*i([ <i>]*>[ <i>]*)*'
 
-###### Correction des balises vides #######                
-def reco_balise(string_content,span):
-    a,b = span
-    if r'<b>' in string_content[a:b]:
-        return 'open_b'
-    elif r'<i>' in string_content[a:b]:
-        return 'open_i'
-    elif r'</b>' in string_content[a:b]:
-        return 'close_b'
-    elif r'</i>' in string_content[a:b]:
-        return 'close_i'
-    else:
-        return 'other'
-        
 
-for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
+for page in root[2].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
     for printspace in page.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}PrintSpace'):
         for textblock in printspace.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextBlock'):
             for textline in textblock.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}TextLine'):
-                count = 0
                 for string in textline.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}String'):
-                    count += 1
-                    # Correction des balises pleines
                     if re.search(patt_b_open,string.attrib['CONTENT']): 
                         string.attrib['CONTENT'] = re.sub(patt_b_open,'<b>',string.attrib['CONTENT'])
-                        #print(string.attrib['CONTENT'])
+##                        #print(string.attrib['CONTENT'])
                     if re.search(patt_i_open,string.attrib['CONTENT']): 
                         string.attrib['CONTENT'] = re.sub(patt_i_open,'<i>',string.attrib['CONTENT'])
                         #print(string.attrib['CONTENT'])
                     if re.search(patt_b_closed,string.attrib['CONTENT']): 
                         string.attrib['CONTENT'] = re.sub(patt_b_closed,'</b>',string.attrib['CONTENT'])
-                       #print(string.attrib['CONTENT'])
+##                        #print(string.attrib['CONTENT'])
                     if re.search(patt_i_closed,string.attrib['CONTENT']): 
                         string.attrib['CONTENT'] = re.sub(patt_i_closed,'</i>',string.attrib['CONTENT'])
                         #print(string.attrib['CONTENT'])
 
-                ###### Correction des balises vides #######        
-                count = 0
-                liste_balises_ligne = []
-                for string in textline.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}String'):
-                    string_content = string.attrib['CONTENT']
-                    matches_balises = pattern_regex_gen.finditer(string_content) # itérateur
-                    for match in matches_balises:
-                        span = match.span()
-                        res = [count, span, reco_balise(string_content,span)]
-                        liste_balises_ligne.append(res)
-                    count += 1
-                long_string = len(liste_balises_ligne)
-                #print(liste_balises_ligne)
-                for i, l in enumerate(liste_balises_ligne):
-                    if l[2] == "other":
-                        if i >= 1 and liste_balises_ligne[i-1][2] == 'open_b':
-                            k = l[0]
-                            a,b = l[1]
-                            string_content = textline[2*k].attrib['CONTENT']
-                            string_content_cor = string_content
-                            N = len(string_content)
-                            string_content_cor = string_content_cor[:-(N-a)] + '</b>' + string_content[b:]
-                            textline[2*k].attrib['CONTENT'] = string_content_cor
-                        elif i >= 1 and liste_balises_ligne[i-1][2] == 'open_i':
-                            k = l[0]
-                            a,b = l[1]
-                            string_content = textline[2*k].attrib['CONTENT']
-                            string_content_cor = string_content
-                            N = len(string_content)
-                            string_content_cor = string_content_cor[:-(N-a)] + '</i>' + string_content[b:]
-                            textline[2*k].attrib['CONTENT'] = string_content_cor                   
-                        elif i < long_string - 1 and liste_balises_ligne[i+1][2] == 'close_b':
-                            k = l[0]
-                            a,b = l[1]
-                            string_content = textline[2*k].attrib['CONTENT']
-                            string_content_cor = string_content
-                            N = len(string_content)
-                            string_content_cor = string_content_cor[:-(N-a)] + '<b>' + string_content[b:]
-                            textline[2*k].attrib['CONTENT'] = string_content_cor
-                        elif i < long_string - 1 and liste_balises_ligne[i+1][2] == 'close_i':
-                            k = l[0]
-                            a,b = l[1]
-                            string_content = textline[2*k].attrib['CONTENT']
-                            string_content_cor = string_content
-                            N = len(string_content)
-                            string_content_cor = string_content_cor[:-(N-a)] + '<i>' + string_content[b:]
-                            textline[2*k].attrib['CONTENT'] = string_content_cor
                         
 
-
-#### Application des trois styles (FONT0, FONT1, FONT2) à tous les éléments <String>	######
+## Application des trois styles (FONT0, FONT1, FONT2) à tous les éléments <String>	######
 
 for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
     for printspace in page.findall('{http://www.loc.gov/standards/alto/v3/alto.xsd}PrintSpace'):
@@ -208,9 +139,9 @@ for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
                         start_i = True
                         string.attrib['CONTENT'] = string.attrib['CONTENT'].replace('<i>', '')
                     if start_b == True:
-                        string.set('STYLEREFS', 'FONT1')
+                        string.set('STYLEREFS', 'FONT0')
                     if start_i == True:
-                        string.set('STYLEREFS', 'FONT2')
+                        string.set('STYLEREFS', 'FONT0')
                     if '</b>' in string.attrib['CONTENT']:
                         start_b = False
                         string.attrib['CONTENT'] = string.attrib['CONTENT'].replace('</b>', '')
@@ -230,3 +161,4 @@ for page in root[3].iter('{http://www.loc.gov/standards/alto/v3/alto.xsd}Page'):
 ##            elt.set(l, pixels)
 
 tree.write(fichier + '_trans.xml', encoding='utf8', pretty_print=True)
+
